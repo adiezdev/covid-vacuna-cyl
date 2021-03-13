615e869276dbe527a66db26f7d5ea58d03a3539e
adilosa95 <adilosa95@gmail.com>
2021-03-13 12:11:53 +0100

    'Coments'

--- src/functions/functionRemaneKeysObj.js
+++ src/functions/functionRemaneKeysObj.js
@@ -1,17 +1,24 @@
+/**
+ * 
+ * @param {*} obj 
+ * @returns 
+ * 
+ * Function that renames the keys of an object according to the group
+ */
 
-
+//Initial clone object functiom
 const clone = (obj) => Object.assign({}, obj);
 
 export const renameKeys = (object, key, newKey) => {
-
+    //pass original objecto to clone
      const clonedObj = clone(object);
-   
+    //Catch original keys
      const targetKey = clonedObj[key];
-   
+    //Delete original key
      delete clonedObj[key];
-   
+    //asing new key
      clonedObj[newKey] = targetKey;
-   
+    //Return new object with new key
      return clonedObj;
    
    };
