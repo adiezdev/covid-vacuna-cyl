a74f1f76326bfe36d46f13748cb207a489faf3ed
adilosa95 <adilosa95@gmail.com>
2021-04-10 17:51:51 +0200

    Pass test VacunasAdministradas

--- src/tests/fixtures/data.js
+++ src/tests/fixtures/data.js
@@ -2,13 +2,13 @@ export const informations = [
     {
         id:  0,
         title:'Personas vacunadas',
-        sumaReduce: '10000000',
+        sumaReduce: 10000000,
         img: './assets/icons/admin_vacuna'
     },
     {
         id:  1,
         title:'Personas con pauta completa',
-        sumaReduce: '10000000',
+        sumaReduce: 10000000,
         img: './assets/icons/vacuna_completa'
     }
 ]
